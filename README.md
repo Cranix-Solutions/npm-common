cb181121cfc8c8359a16cfd651e7365680cfc577
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2025-02-07 09:39:15 +0100

    Add menu list

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # npm-common
+
+Common models services and constants for the cranix gui packages: cranix-web and cranix-app
+After all changes:
+
+# Increase version in package.json
+# `npm run build`
+# If no problems accured `git commit` otherwise fix it and test it again.
+# `npm login`
+# `npm publish`
--- src/models/constants.ts
+++ src/models/constants.ts
@@ -24,3 +24,81 @@ export const mathToolbarOptions = [
     [{ 'align': [] }],
     ['link','image']
 ];
+
+export const defAppPages = [
+    {
+      title: 'Customers',
+      url: '/pages/cephalix/customers',
+      icon: 'albums'
+    },
+    {
+      title: 'Institutes',
+      url: '/pages/cephalix/institutes/all',
+      icon: 'business'
+    },
+    {
+      title: 'Tickets',
+      url: '/pages/cephalix/tickets',
+      icon: 'pricetags'
+    },
+    {
+      title: 'Groups',
+      url: '/pages/cranix/groups',
+      icon: 'people'
+    },
+    {
+      title: 'Users',
+      url: '/pages/cranix/users',
+      icon: 'person'
+    },
+    {
+      title: 'HWConfs',
+      url: '/pages/cranix/hwconfs',
+      icon: 'file-tray-stacked'
+    },
+    {
+      title: 'Rooms',
+      url: '/pages/cranix/rooms',
+      icon: 'room'
+    },
+    {
+      title: 'Devices',
+      url: '/pages/cranix/devices',
+      icon: 'desktop'
+    },
+    {
+      title: 'Softwares',
+      url: '/pages/cranix/softwares',
+      icon: 'software'
+    },
+    {
+      title: 'System',
+      url: '/pages/cranix/system',
+      icon: 'settings'
+    },
+    {
+      title: 'Security',
+      url: '/pages/cranix/security',
+      icon: 'security'
+    },
+    {
+      title: 'Lessons',
+      url: '/pages/edu/lessons',
+      icon: 'school'
+    },
+    {
+      title: 'Profile',
+      url: '/pages/cranix/profile',
+      icon: 'man'
+    },
+    {
+      title: 'MyGroups',
+      url: '/pages/cranix/mygroups',
+      icon: "people"
+    },
+    {
+      title: "Informations",
+      url: '/pages/cranix/informations',
+      icon: 'library'
+    }
+];
